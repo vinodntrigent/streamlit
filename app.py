1d5e4b51068e2bbc10ea968c3d91d9dd8b828584
vinodntrigent <vinod_n@trigent.com>
2025-11-27 15:30:21 +0530

    test2

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 # Page configuration
 st.set_page_config(
-    page_title="Haystack AI Product Assistant",
+    page_title="Haystack AI Product Assistant1",
     page_icon="☁️",
     layout="wide",
     initial_sidebar_state="collapsed"
